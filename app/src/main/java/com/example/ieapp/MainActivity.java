6039c31bb6f1d5b5dc396d9dd8de540415d4d024
prakritigoyal19 <prakritigoyal19@gmail.com>
2020-10-12 17:41:02 +0530

    Read the question and option from files
    This commit adds functionality to read the question and options from files and
    display at appropriate places.
    
    Signed-off-by: prakritigoyal19 <prakritigoyal19@gmail.com>

--- app/src/main/java/com/example/ieapp/MainActivity.java
+++ app/src/main/java/com/example/ieapp/MainActivity.java
@@ -13,7 +13,8 @@ import java.io.InputStream;
 public class MainActivity extends AppCompatActivity {
 
     Button b_read;
-
+    Button button;
+    Button button2;
     TextView tv_text;
 
     @Override
@@ -21,21 +22,46 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        b_read = (Button) findViewById(R.id.b_read);
-        tv_text = (TextView) findViewById(R.id.tv_text);
+        b_read = findViewById(R.id.choice1);
+        button = findViewById(R.id.choice2);
+        button2 = findViewById(R.id.choice3);
+        tv_text = findViewById(R.id.question);
 
         String text = "";
         try {
-            InputStream is = getAssets().open("Question 1.txt");
+            InputStream is = getAssets().open("Question 2.txt");
             int size = is.available();
             byte[] buffer = new byte[size];
             is.read(buffer);
             is.close();
             text = new String(buffer);
+
         } catch (IOException ex) {
             ex.printStackTrace();
         }
-        tv_text.setText(text);
+        String screen = "";
+        int index = text.indexOf("split_to_button");
+        if (index != -1) {
+            screen = text.substring(0, index);
+        }
+        String b1 = "";
+        int i = text.indexOf("split_to_button", index + 1);
+        if (i != -1 && index != -1) {
+            b1 = text.substring(index + 15, i);
+        }
+        index = i;
+        String b2 = "";
+        i = text.indexOf("split_to_button", index +1 );
+        if (i != -1 && index != -1) {
+            b2 = text.substring(index + 15, i);
+        }
+        index = i;
+        String b3 = "";
+        b3 = text.substring(index + 15);
+        tv_text.setText(screen);
+        b_read.setText(b1);
+        button.setText(b2);
+        button2.setText(b3);
         }
     }
 
