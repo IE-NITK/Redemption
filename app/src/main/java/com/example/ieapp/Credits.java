ec94cca1410609f2172ae948cb110521122784ab
Dwaipayan Munshi <53687927+dwaipayan05@users.noreply.github.com>
2021-03-14 11:51:43 +0530

    Revamped the FrontEnd

---
+++ app/src/main/java/com/example/ieapp/Credits.java
@@ -0,0 +1,61 @@
+package com.example.ieapp;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Credits extends AppCompatActivity {
+
+
+
+    private TextView textView;
+    private Animation animation;
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        Bundle savedInstancesState = new Bundle();
+        super.onCreate(savedInstancesState);
+        setContentView(R.layout.credits);
+        Button b5=(Button)findViewById(R.id.b5);
+        b5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Credits.this, HomeActivity.class));
+
+            }
+        });
+        animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.credits);
+
+        textView=(TextView)findViewById(R.id.textView);
+
+        String n0 = "\n";
+        String n1 = getString(R.string.credits_heading);
+        String n2 = getString(R.string.lead);
+        String n3 = getString(R.string.lead_name1);
+        String n4 = getString(R.string.lead_name2);
+        String n5 = getString(R.string.story);
+        String n6 = getString(R.string.story_contri1);
+        String n7 = getString(R.string.story_contri2);
+        String n8 = getString(R.string.story_contri3);
+        String n9 = getString(R.string.app);
+        String n10 = getString(R.string.app_contri1);
+        String n11 = getString(R.string.app_contri2);
+        String n12 = getString(R.string.specialthanks);
+        String n13 = getString(R.string.woc_contri1);
+        String n14 = getString(R.string.woc_contri2);
+
+
+        textView.setText(n1 + n0 + n0 + n0 + n2 + n0 + n3 + n0 + n4 + n0 + n0 + n5 + n0 + n6 + n0 +
+                n7 + n0 + n8 + n0 + n0 + n9 + n0 + n10 +
+                n0 + n11 + n0 + n0 + n12 + n0 + n13 + n0 + n14);
+        textView.startAnimation( animation);
+
+    }
+}
---
+++ app/src/main/java/com/example/ieapp/HomeActivity.java
@@ -0,0 +1,86 @@
+package com.example.ieapp;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class HomeActivity extends AppCompatActivity {
+    Button quit;
+    Button start;
+    private ImageButton b;
+    Button settings;
+    Button play;
+    MediaPlayer ring;
+    Button resume;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        start = (Button) findViewById(R.id.button);
+        resume = (Button) findViewById(R.id.button8);
+
+        SharedPreferences sh = getSharedPreferences("MySharedPref", Context.MODE_PRIVATE);
+        final String id = sh.getString("id", "ID01");
+        final int score = sh.getInt("score", 0);
+
+        resume.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(HomeActivity.this, MainActivity.class);
+                i.putExtra("id", id);
+                i.putExtra("score", score);
+                startActivity(i);
+            }
+        });
+
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(HomeActivity.this, MainActivity.class);
+                i.putExtra("id", "ID01");
+                i.putExtra("score", 0);
+                startActivity(i);
+            }
+        });
+
+        if (!id.equals("ID01")) {
+            start.setVisibility(View.GONE);
+            resume.setVisibility(View.VISIBLE);
+        } else {
+            resume.setVisibility(View.GONE);
+            start.setVisibility(View.VISIBLE);
+        }
+
+        settings = (Button) findViewById(R.id.button3);
+        settings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), Settings.class));
+            }
+        });
+
+        quit = (Button) findViewById(R.id.Button6);
+        quit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+        b = (ImageButton) findViewById(R.id.button4);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), Credits.class));
+            }
+        });
+    }
+}
+
